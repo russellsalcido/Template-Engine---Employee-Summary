fdf5ec056d7753ed3eeac2186298c1b3967570ad
Russell Salcido <russell.salcido@gmail.com>
2020-06-02 20:43:01 -0700

    Built inquirer questions list

--- Develop/app.js
+++ Develop/app.js
@@ -4,15 +4,56 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const prompts = new Rx.Subject();
+inquirer.prompt(prompts);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+inquirer.prompt([
+    {
+        type: "checkbox",
+        name: "name",
+        message: "You are about to begin building an engineer team roster with ease. An engineering team consists of a manager, and any number of engineers and interns. To begin, please enter the name of the employee you would like to add to the roster:",
+    },
+    {
+        name: "id",
+        message: "Employee ID:",
+    },
+    {
+        name: "email",
+        message: "Employee email address:",
+    },
+    {
+        name: "role",
+        message: "Employee role:",
+        choices: [
+          "Manager", 
+            prompts.next({
+                name: "Office phone number?"
+            }),
+            prompts.complete(),
+          new inquirer.Separator(), 
+          "Engineer", 
+            prompts.next({
+                name: "Github username?"
+            }),
+            prompts.complete(),
+          new inquirer.Separator(), 
+          "Intern",
+            prompts.next({
+                name: "Github username?"
+            }),
+            prompts.complete(),
+        ]
+    },
+]).then(function(data) {
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
