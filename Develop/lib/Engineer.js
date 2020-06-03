e468883c3228539defb4b5d12d68e1b25c7d1fe9
Russell Salcido <russell.salcido@gmail.com>
2020-06-02 19:24:21 -0700

    created sub class for Engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,14 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require(".Employee/");
+
+class Engineer extends Employee {
+  constructor(name, email, id, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+
+}
+
+getGithub()
+
+getRole('Engineer') // Overridden to return 'Engineer'
