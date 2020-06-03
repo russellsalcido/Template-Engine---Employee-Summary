7ed724779c77930cbeebf3b4a15052a87af470f1
Russell Salcido <russell.salcido@gmail.com>
2020-06-02 19:08:30 -0700

    built employee super class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+	constructor(id) {
+        // name
+        this.name = name;
+        // id
+		this.id = id;
+        // email
+		this.email = email;
+	}
+
+	printInfo() {
+		console.log(`This employee's name is ${this.name}`);
+        console.log(`This employee has an id of ${this.id}`);
+        console.log(`This employee has an email of ${this.id}`);
+	}
+}
+getName()
+getId()
+getEmail()
+getRole() // Returns 'Employee'
+
+module.exports = Employee;
