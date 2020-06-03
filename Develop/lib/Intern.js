c5c286c3276c5bd92c2326c54b41988cd29f1292
Russell Salcido <russell.salcido@gmail.com>
2020-06-02 19:26:56 -0700

    created intern sub-class

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,14 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require(".Employee/");
+
+class Intern extends Employee {
+  constructor(name, email, id, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+
+}
+
+getSchool()
+
+getRole('Intern') // Overridden to return 'Intern'
