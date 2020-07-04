5d370753fa70e6358e07857259c7ef6ce1ca8010
Russell Salcido <russell.salcido@gmail.com>
2020-07-03 20:58:09 -0700

    Nuked, restart, init commit

--- app.js
+++ app.js
@@ -4,76 +4,20 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const prompts = new Rx.Subject();
-const Rx = require('rx');
-const prompts = new Rx.Subject();
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
-inquirer.prompt([
-    {
-        type: "checkbox",
-        name: "name",
-        message: "You are about to begin building an engineer team roster with ease. An engineering team consists of a manager, and any number of engineers and interns. To begin, please enter the name of the employee you would like to add to the roster:",
-    },
-    {
-        name: "id",
-        message: "Employee ID:",
-    },
-    {
-        name: "email",
-        message: "Employee email address:",
-    },
-    {
-        name: "role",
-        message: "Employee role:",
-        choices: [
-          "Manager", 
-            prompts.next({
-                name: "officeNumber",
-                message: "Office phone number?",
-                },
-                {
-                name: "restart",
-                message: "Would you like to add another employee?"
-            }),
-            prompts.complete(),
-          new inquirer.Separator(), 
-          "Engineer", 
-            prompts.next({
-                name: "github",
-                message: "Github username?",
-                },
-                {
-                name: "restart",
-                message: "Would you like to add another employee?"
-            }),
-            prompts.complete(),
-          new inquirer.Separator(), 
-          "Intern",
-            prompts.next({
-                name: "school",
-                message: "What school do they attend?"
-                },
-                {
-                name: "restart",
-                message: "Would you like to add another employee?"
-            }),
-            prompts.complete(),
-        ]
-    },
-])
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-// .render();
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
